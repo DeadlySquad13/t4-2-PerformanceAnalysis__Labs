9eb6df3c10b30aa7c9da10a5232e7bb14cef4e96
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-03-08 15:54:39 +0300

    [Refactor](Lab3): Formatted code with styler.

--- Lab3/PerformanceAnalysis__Lab3.ju.r
+++ Lab3/PerformanceAnalysis__Lab3.ju.r
@@ -1,23 +1,23 @@
 # %%
-variant<-5
-set.seed(variant) 
-k<-sample(c(4:9), 1)
-pp1<-runif(4)
-pp2<-runif(3)
-pp3<-runif(2)
-p1<-pp1/sum(pp1)
-p2<-c(c(0),pp2/sum(pp2))
-p3<-c(c(0,0),pp3/sum(pp3))
-p4<-c(0,0,0,1)
-P<-data.frame()
-P<-rbind(P,p1)
-P<-rbind(P,p2)
-P<-rbind(P,p3)
-P<-rbind(P,p4)
-rownames(P)<-c("p1","p2","p3","p4")
-colnames(P)<-c("","","","")
+variant <- 5
+set.seed(variant)
+k <- sample(c(4:9), 1)
+pp1 <- runif(4)
+pp2 <- runif(3)
+pp3 <- runif(2)
+p1 <- pp1 / sum(pp1)
+p2 <- c(c(0), pp2 / sum(pp2))
+p3 <- c(c(0, 0), pp3 / sum(pp3))
+p4 <- c(0, 0, 0, 1)
+P <- data.frame()
+P <- rbind(P, p1)
+P <- rbind(P, p2)
+P <- rbind(P, p3)
+P <- rbind(P, p4)
+rownames(P) <- c("p1", "p2", "p3", "p4")
+colnames(P) <- c("", "", "", "")
 View(P)
-print(paste("k =",as.character(k)))
+print(paste("k =", as.character(k)))
 
 # %%
 # Получаем значение, соответствующее отрезку.
@@ -27,7 +27,7 @@ print(paste("k =",as.character(k)))
 # За отрезки отвечает параметр p, хранящий последовательность длин.
 get_value_in_range <- function(v, X, p) {
     border <- 0
-    for (i in 1:length(p)) {
+    for (i in seq_along(p)) {
         border <- border + p[i]
         if (v < border) {
             return(X[i])
@@ -38,12 +38,10 @@ get_value_in_range <- function(v, X, p) {
 
 # %%
 make_step <- function(current_state) {
-    possible_states <- P[current_state,]
+    possible_states <- P[current_state, ]
 
     return(get_value_in_range(runif(1), 1:4, possible_states))
 }
-
-make_step(1)
 
 # %%
 walk <- function(starting_state, times) {
@@ -81,8 +79,8 @@ S2 <- test_walk(first_state, k - 1)
 S3 <- test_walk(first_state, k)
 
 # %%
-get_probability_of <-function(States, state) {
-    number_of_states <- length(States[States==state])
+get_probability <- function(States, state) {
+    number_of_states <- length(States[States == state])
     number_of_all_states <- length(States)
 
     return(number_of_states / number_of_all_states)
@@ -90,7 +88,9 @@ get_probability_of <-function(States, state) {
 
 # %%
 get_probabilities_to_stay <- function(States) {
-    return(lapply(1:length(P), function(state) get_probability_of(States, state)))
+    return(
+        lapply(seq_along(P), function(state) get_probability(States, state))
+    )
 }
 
 # %%
@@ -106,16 +106,16 @@ get_probabilities_to_stay(S3)
 # # Задание 2
 
 # %%
-variant<-5
-set.seed(variant) 
-k<-sample(c(10:25),1)
-t1<-sample(c(14:20),1)
-t2<-sample(c(2:5),1)
-View(data.frame(k,t1,t2))
+variant <- 5
+set.seed(variant)
+k <- sample(c(10:25), 1)
+t1 <- sample(c(14:20), 1)
+t2 <- sample(c(2:5), 1)
+View(data.frame(k, t1, t2))
 
 # %%
-if (!require('simmer')) {
-    install.packages('simmer')
+if (!require("simmer")) {
+    install.packages("simmer")
 }
 library(simmer)
 
@@ -126,21 +126,23 @@ env
 
 # %%
 programmers <- trajectory("programmers' path") %>%
-  ## add an intake activity 
-  seize("server", 1) %>%
-  timeout(function() rnorm(1, 5)) %>%
-  release("server", 1)
+    ## add an intake activity
+    seize("server", 1) %>%
+    timeout(function() rnorm(1, 5)) %>%
+    release("server", 1)
 
 # %%
 env %>%
-  add_resource("server", 1) %>%
-  add_generator("programmers", programmers, function() 10)
-                #, function() rnorm(1, 0.1, 2))
+    add_resource("server", 1) %>%
+    add_generator("programmers", programmers, function() {
+        return(10)
+    })
+# , function() rnorm(1, 0.1, 2))
 
 # %%
-env %>% 
-  run(50) %>% 
-  now()
+env %>%
+    run(50) %>%
+    now()
 env %>% peek(3)
 
 # %%
