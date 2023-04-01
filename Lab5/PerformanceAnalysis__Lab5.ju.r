f074b699adebcfc5eb8894444c12bd9c83d9d908
DeadlySquad13 <46250621+DeadlySquad13@users.noreply.github.com>
2023-04-01 18:58:32 +0300

    [Enh](Theoretical): Added deSolve and calculated most of the characteristics.

--- Lab5/PerformanceAnalysis__Lab5.ju.r
+++ Lab5/PerformanceAnalysis__Lab5.ju.r
@@ -15,7 +15,7 @@
 # условия $P_0(0) = 1$.
 
 # %%
-Variant <- 1
+Variant <- 5
 set.seed(Variant)
 m <- sample(c(4:18), 1)
 
@@ -56,7 +56,7 @@ View(data.frame(lambda, mu, nu, gamma, k, m, t))
 # - $S_{m+1}$ - СМО вышла из строя, в очереди нет задач;
 # - $S_{m+2}$ - СМО вышла из строя, в очереди одна задача;
 # - ...
-# - $S_{2m + 1}$ - СМО вышла из строя, в очереди m задач.
+# - $S_{2m}$ - СМО вышла из строя, в очереди m задач.
 
 # %% [markdown]
 # Тогда граф состояний будет выглядеть:
@@ -66,27 +66,194 @@ View(data.frame(lambda, mu, nu, gamma, k, m, t))
 # По графу составим уравнения Колмогорова:
 # $$
 # \begin{cases}
-# \frac{dP_0(t)}{dt} = -(\lambda + \nu)P_0(t) + \mu\cdot P_1(t) + \gamma\cdot P_4(t) \\
-# \frac{dP_1(t)}{dt} = \lambda\cdot P_0(t)-(\lambda + \mu + \nu)\cdot P_1(t) + \mu\cdot P_2(t) + \gamma\cdot P_5(t) \\
-# \frac{dP_2(t)}{dt} = \lambda\cdot P_1(t)-(\lambda + \mu + \nu)\cdot P_2(t) + \mu\cdot P_3(t) + \gamma\cdot P_6(t) \\
-# \frac{dP_3(t)}{dt} = \lambda\cdot P_2(t)-(\mu + \nu)\cdot P_3(t) \\
-# \frac{dP_4(t)}{dt} = \nu\cdot P_0(t)-(\lambda + \gamma)\cdot P_4(t) \\
-# \frac{dP_5(t)}{dt} = \nu\cdot P_1(t) + \lambda\cdot P_4(t)-(\lambda + \gamma)\cdot P_5(t) \\
-# \frac{dP_6(t)}{dt} = \nu\cdot P_2(t) + \nu\cdot P_3(t) + \lambda\cdot P_5(t)-\gamma\cdot P_6(t)
+# \frac{dP_0(t)}{dt} = -(\lambda + \nu)P_0(t) + \mu\cdot P_1(t) + \gamma \cdot P_6(t) \\
+# \frac{dP_1(t)}{dt} = -(\lambda + \nu + \mu)P_1(t) + \lambda \cdot P_0(t) + \mu \cdot P_2(t) + \gamma \cdot P_7(t) \\
+# \frac{dP_2(t)}{dt} = -(\lambda + \nu + \mu)P_2(t) + \lambda \cdot P_1(t) + \mu \cdot P_3(t) + \gamma \cdot P_8(t) \\
+# \frac{dP_3(t)}{dt} = -(\lambda + \nu + \mu)P_3(t) + \lambda \cdot P_2(t) + \mu \cdot P_4(t) + \gamma \cdot P_9(t) \\
+# \frac{dP_4(t)}{dt} = -(\lambda + \nu + \mu)P_4(t) + \lambda \cdot P_3(t) + \mu \cdot P_5(t) + \gamma \cdot P_{10}(t) \\
+# \frac{dP_5(t)}{dt} = -(\nu + \mu)P_5(t) + \lambda \cdot P_4(t) \\
+# \frac{dP_6(t)}{dt}  = -(\gamma + \lambda)P_6(t) + \nu \cdot P_0(t) \\
+# \frac{dP_7(t)}{dt}  = -(\gamma + \lambda)P_7(t) + \lambda \cdot P_6(t) + \nu \cdot P_1(t) \\
+# \frac{dP_8(t)}{dt}  = -(\gamma + \lambda)P_8(t) + \lambda \cdot P_7(t) + \nu \cdot P_2(t) \\
+# \frac{dP_9(t)}{dt}  = -(\gamma + \lambda)P_9(t) + \lambda \cdot P_{8}(t) + \nu \cdot P_3(t) \\
+# \frac{dP_{10}(t)}{dt} = -\gamma \cdot P_{10}(t) + \lambda \cdot P_{9}(t) + \nu \cdot P_4(t) + \nu \cdot P_5(t)
 # \end{cases}
 # $$
 
 # и уравнение нормировки:
 # $$
-# P_0(t) + P_1(t) + P_2(t) + P_3(t) + P_4(t) + P_5(t) + P_6(t) = 1
+# P_0(t) + P_1(t) + P_2(t) + P_3(t) + P_4(t) + P_5(t) + P_6(t) + P_7(t) + P_8(t) + P_9(t) + P_{10}(t) = 1
 # $$
 
+# %% [markdown]
+# Решим эти уравнения с помощью пакета deSolve
+# (см. 75 стр. [документации](chrome-extension://gfbliohnnapiefjpjlpjnehglfpaknnc/pages/pdf_viewer.html?r=https://cran.r-project.org/web/packages/deSolve/deSolve.pdf)):
+
+# %%
+if (!require("deSolve")) {
+    install.packages("deSolve")
+}
+library(deSolve)
+
+ode_system_equations <- function(Time, State, Pars) {
+    with(as.list(c(State, Pars)), {
+        dP_0 <- -(lambda + nu) * P_0 + mu * P_1 + gamma * P_6
+        dP_1 <- -(lambda + nu + mu) * P_1 + lambda * P_0 + mu * P_2 + gamma * P_7
+        dP_2 <- -(lambda + nu + mu) * P_2 + lambda * P_1 + mu * P_3 + gamma * P_8
+        dP_3 <- -(lambda + nu + mu) * P_3 + lambda * P_2 + mu * P_4 + gamma * P_9
+        dP_4 <- -(lambda + nu + mu) * P_4 + lambda * P_3 + mu * P_5 + gamma * P_10
+        dP_5 <- -(nu + mu) * P_5 + lambda * P_4
+        dP_6 <- -(gamma + lambda) * P_6 + nu * P_0
+        dP_7 <- -(gamma + lambda) * P_6 + lambda * P_5 + nu * P_0
+        dP_8 <- -(gamma + lambda) * P_7 + lambda * P_6 + nu * P_1
+        dP_8 <- -(gamma + lambda) * P_8 + lambda * P_7 + nu * P_2
+        dP_9 <- -(gamma + lambda) * P_9 + lambda * P_8 + nu * P_3
+        dP_10 <- -gamma * P_10 + lambda * P_9 + nu * P_4 + nu * P_5
+
+        # Specifying list of derivatives.
+        return(
+            list(
+                c(dP_0, dP_1, dP_2, dP_3, dP_4, dP_5, dP_6, dP_7, dP_8, dP_9, dP_10)
+            )
+        )
+    })
+}
+
+# %%
+pars <- c(norm_equation = 1)
+yini <- c(
+    P_0 = 1,
+    P_1 = 0,
+    P_2 = 0,
+    P_3 = 0,
+    P_4 = 0,
+    P_5 = 0,
+    P_6 = 0,
+    P_7 = 0,
+    P_8 = 0,
+    P_9 = 0,
+    P_10 = 0
+)
+
+accuracy <- 0.001
+times <- seq(0, 1, by = accuracy)
+output <- ode(yini, times, ode_system_equations, pars)
+output
+
+# %% [markdown]
+# Вычислим индекс строки, которую нужно взять для получения результата при
+# заданом в условии $t$:
+
+# %%
+precision_of_accuracy <- 3
+t_index <- round(t, precision_of_accuracy) * 10^precision_of_accuracy + 1
+print(t_index)
+results <- output[t_index, 1:12]
+results
+
+# %%
+# index - index of P.
+get_P <- function(index) {
+    return(as.numeric(results)[2 + index])
+}
+
+
+# %% [markdown]
 # ### Вероятность простоя
+# Вероятность простоя равна $P_0$:
+
+# %%
+get_P(0)
+
+# %% [markdown]
 # ### Вероятность образования очереди
+# Вероятность образования очереди обратной величине от суммы $P_i$, соответствующим состояниям,
+# в которых очередь пуста.
+# $$
+# 1 - (P_0 + P_1 + P_{m + 1})
+# $$
+
+# %%
+1 - get_P(0) + get_P(1) + get_P(m + 1)
+
+# %% [markdown]
 # ### Абсолютную пропускную способность
+# $\lambda'=\lambda\cdot(1-P_m(t)-P_{2m}(t))$
+
+# %%
+absolute_intensity <- lambda * (1 - get_P(m) - get_P(2 * m))
+absolute_intensity
+
+# %% [markdown]
 # ### Среднюю длину очереди
+# Для вычисления средней длины очереди просуммируем произведения вероятностей
+# на соответствующие этим вероятностям длины очередей.
+# $$
+# L_{\text{оч}} = 1 \cdot P_2(t) + 2 \cdot P_3(t) + 3 \cdot P_4(t) + \text{...} + (m-1) \cdot P_m
+# + 1 \cdot P_{m+2}(t) + 2 \cdot P_{m+3}(t) + \text{...} + (m - 1) \cdot P_{2m}(t)
+# $$
+
+# %%
+get_queue_length_for_P <- function(index) {
+    if (index < 1) {
+        return(0)
+    }
+
+    return(index - 1)
+}
+
+get_P_product <- function(index) {
+    return(get_P(index) * get_queue_length_for_P(index))
+}
+
+mean_length <- sum(unlist(
+    lapply(c(2:m), get_P_product)
+)) + sum(unlist(
+    lapply(c((m + 2):(2 * m)), get_P_product)
+))
+mean_length
+
+# %% [markdown]
 # ### Среднее время нахождения заявок в системе
+# $$
+# W_{\text{оч}}=\frac{L_{\text{оч}}}{\lambda'}
+# $$
+
+# %%
+mean_length / absolute_intensity
+
+# %% [markdown]
 # ### Среднее число заявок в системе
+# Для вычисления средней длины очереди просуммируем произведения вероятностей
+# на соответствующие этим вероятностям длины очередей + количество заявок на
+# обслуживании.
+# $$
+# L_{\text{оч}} = 1 \cdot P_1(t) + 2 \cdot P_2(t) + 3 \cdot P_3(t) + 4 \cdot P_4(t) + \text{...} + m \cdot P_m
+# + 1 \cdot P_{m+2}(t) + 2 \cdot P_{m+3}(t) + \text{...} + (m) \cdot P_{2m}(t)
+# $$
+
+# %%
+get_queue_length_for_P <- function(index) {
+    if (index < 1) {
+        return(0)
+    }
+
+    return(index - 1)
+}
+
+get_P_product <- function(index) {
+    return(get_P(index) * get_queue_length_for_P(index))
+}
+
+mean_length <- sum(unlist(
+    lapply(c(2:m), get_P_product)
+)) + sum(unlist(
+    lapply(c((m + 2):(2 * m)), get_P_product)
+))
+mean_length
+
+# %%
+
 # ### Среднее время нахождения в очереди
 
 # %%
